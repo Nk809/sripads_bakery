be7cecd1d8b1dec7d824c10f247e77ccd71511c5
Nishikanta Biswal <nk._biswal@localhost.localdomain>
2026-07-28 15:34:03 +0530

    Import UploadedFile in bakery/views.py to fix NameError

--- bakery/views.py
+++ bakery/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 import json
-from .models import Category, Product, CartItem, Coupon, BakerySettings
+from .models import Category, Product, CartItem, Coupon, BakerySettings, UploadedFile
 from feedback.models import Feedback
 
 def home(request):
